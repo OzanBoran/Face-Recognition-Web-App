cd5ae014c55990d2d11cde8c24b788e6160642ea
Ozan <OzanBoran@users.noreply.github.com>
2020-10-12 16:24:17 +0300

    Updates

---
+++ precache-manifest.d16c3fd63c2a40858031f7920f0f0519.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25febe801b57b5805b973997e163f40f",
+    "url": "/OzanBoran/yuztanima/index.html"
+  },
+  {
+    "revision": "75b9f701c5e5d2bee81c",
+    "url": "/OzanBoran/yuztanima/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "62738dfb393512958c84",
+    "url": "/OzanBoran/yuztanima/static/css/main.289e242f.chunk.css"
+  },
+  {
+    "revision": "75b9f701c5e5d2bee81c",
+    "url": "/OzanBoran/yuztanima/static/js/2.1d858334.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/OzanBoran/yuztanima/static/js/2.1d858334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62738dfb393512958c84",
+    "url": "/OzanBoran/yuztanima/static/js/main.ca337c8e.chunk.js"
+  },
+  {
+    "revision": "a504da9b1d88c965feac",
+    "url": "/OzanBoran/yuztanima/static/js/runtime-main.3bf98d67.js"
+  },
+  {
+    "revision": "371f1f9ead26096ad21c8c13a38161bd",
+    "url": "/OzanBoran/yuztanima/static/media/electric-brain.371f1f9e.jpg"
+  }
+]);
